c98c7a86e06affe4a4bc72e2282ff9535ca8861e
Dennis Johnson <densarjohn@gmail.com>
2020-11-19 17:46:37 +0530

    Week 6, finished drawing learning curves

--- machine-learning-ex5/ex5/learningCurve.m
+++ machine-learning-ex5/ex5/learningCurve.m
@@ -50,17 +50,13 @@ error_val   = zeros(m, 1);
 %           
 %       end
 %
+theta= [1;1];
 
-% ---------------------- Sample Solution ----------------------
-
-
-
-
-
-
-
-% -------------------------------------------------------------
-
+for i = 1:m
+	theta = trainLinearReg(X(1:i, :), y(1:i), 0);
+	error_train(i) = linearRegCostFunction(X(1:i, :), y(1:i), theta, 0);
+	error_val(i)   = linearRegCostFunction(Xval, yval, theta, 0);
+endfor
 % =========================================================================
 
 end
